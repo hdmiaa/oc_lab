db398cd8206847436b2f7c5b858980189ddd4446
Mia <mia.herrera@uabc.edu.mx>
2025-03-28 15:58:37 +0000

    commit

--- P4/p4f.asm
+++ P4/p4f.asm
@@ -4,9 +4,14 @@
 section	.text
 	global _start       ;referencia para inicio de programa
 	
-_start:                   
+_start:      
+
 	mov edx, msg		; edx = dirección de la cadena msg
 	call puts			; imprime cadena msg terminada en valor nulo (0)
+
+	mov esi, 19
+	mov byte [edx + esi * 1], '%'	
+	call puts 
 
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
