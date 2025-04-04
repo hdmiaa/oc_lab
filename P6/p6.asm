d9f6fef133fdda0c384093b49f71544b64390a22
Mia <mia.herrera@uabc.edu.mx>
2025-04-04 18:45:55 +0000

    commit

--- P6/p6.asm
+++ P6/p6.asm
@@ -1,21 +1,17 @@
-%include "../LIB/pbin.o"
-
 section .text
 
     global _start
 
     _start:
 
-    mov eax, 0x22446688     ;inciso a
-    ror eax, 8              ;Rotacion de derecha a izquierda
-    cf 8
-    pBin_dw                 ; imprime el valor de eax
+    mov eax, 0x22446688     ;inciso a             
+    extern pBin_dw          ;indica el procedimiento externo 
+   
+    mov al, 10              ;salto de linea
+	call pBin_dw            ; imprime en binario 
 
+    mov cx, 0x3F48
 
-    mov al, 10
-	call putchar
 
 	mov eax, 1          ; sys_exit (termina el programa)
 	int 0x80            ; Llamada al kernel
-
-
