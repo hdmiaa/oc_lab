9fe2110211202735d81ac42ae2ed73b4802083e9
Mia <mia.herrera@uabc.edu.mx>
2025-04-09 17:54:09 +0000

    commit

--- P6/p6.asm
+++ P6/p6.asm
@@ -1,11 +1,14 @@
+%include "../LIB/pc_iox.inc"
 section .text
 
     global _start
 
     _start:
 
-    mov eax, 0x22446688     ;inciso a             
-    extern pBin_dw          ;indica el procedimiento externo 
+    mov eax, 0x22446688     ;inciso a      
+    ror eax, 1
+
+    call        
    
     mov al, 10              ;salto de linea
 	call pBin_dw            ; imprime en binario 
